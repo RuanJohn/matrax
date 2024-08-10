76858aa4be9a569b41789d153e9145e3773cc544
Ruan de Kock <ruanjdekock@gmail.com>
2024-08-10 14:31:01 +0200

    feat: add general payoff matrix game

---
+++ matrax/generalised_matrix_game.py
@@ -0,0 +1,186 @@
+# Copyright 2023 InstaDeep Ltd. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import functools
+from typing import Tuple
+
+import chex
+import jax
+import jax.numpy as jnp
+from jumanji import specs
+from jumanji.env import Environment
+from jumanji.types import TimeStep, restart, termination, transition
+
+from matrax.types import Observation, State
+from matrax.utils.observation import create_random_matrix
+
+
+class MatrixGame(Environment[State]):
+    """JAX implementation of an n-player matrix game environment with a shared payoff matrix."""
+
+    def __init__(
+        self,
+        key_integer: int,
+        num_agents: int,
+        num_actions: int,
+        keep_state: bool = False,
+        time_limit: int = 1,
+    ):
+        """Instantiates a `MatrixGame` environment.
+
+        Args:
+            payoff_matrix: an array of shape (action_space, action_space, ..., action_space)
+                where there are `num_agents` dimensions, representing the shared payoff matrix.
+            keep_state: whether to keep state by giving agents the actions of all players in
+                the previous round as observations. Defaults to True.
+            time_limit: the maximum step limit allowed within the environment.
+                Defaults to 500.
+        """
+        self.payoff_matrix, self.max_pos, self.min_pos = create_random_matrix(
+            key_integer, num_agents, num_actions
+        )
+        self.keep_state = keep_state
+
+        # Number of agents is inferred from the dimensions of the payoff matrix
+        self.num_agents = self.payoff_matrix.ndim
+        self.num_actions = self.payoff_matrix.shape[0]
+
+        self.time_limit = time_limit
+
+    def __repr__(self) -> str:
+        return f"MatrixGame(\n" f"\tpayoff_matrix={self.payoff_matrix!r},\n" ")"
+
+    def reset(self, key: chex.PRNGKey) -> Tuple[State, TimeStep[Observation]]:
+        """Resets the environment.
+
+        Args:
+            key: random key used to reset the environment since it is stochastic.
+
+        Returns:
+            state: State object corresponding to the new state of the environment.
+            timestep: TimeStep object corresponding the first timestep returned by the environment.
+        """
+        # create environment state
+        state = State(
+            step_count=jnp.array(0, int),
+            key=key,
+        )
+        dummy_actions = jnp.ones((self.num_agents,), int) * -1
+
+        # collect first observations and create timestep
+        agent_obs = jax.vmap(
+            functools.partial(self._make_agent_observation, dummy_actions)
+        )(jnp.arange(self.num_agents))
+        observation = Observation(
+            agent_obs=agent_obs,
+            step_count=state.step_count,
+        )
+        timestep = restart(observation=observation, shape=self.num_agents)
+        return state, timestep
+
+    def step(
+        self,
+        state: State,
+        actions: chex.Array,
+    ) -> Tuple[State, TimeStep[Observation]]:
+        """Perform an environment step.
+
+        Args:
+            state: State object containing the dynamics of the environment.
+            actions: Array containing actions of each agent.
+
+        Returns:
+            state: State object corresponding to the next state of the environment.
+            timestep: TimeStep object corresponding the timestep returned by the environment.
+        """
+
+        def compute_reward(actions: chex.Array) -> chex.Array:
+            reward_idx = tuple(actions)
+            return self.payoff_matrix[reward_idx].astype(float)
+
+        # Compute the same reward for all agents (since the payoff matrix is shared)
+        reward = compute_reward(actions)
+        rewards = jnp.full((self.num_agents,), reward)
+
+        # construct timestep and check environment termination
+        steps = state.step_count + 1
+        done = steps >= self.time_limit
+
+        # compute next observation
+        agent_obs = jax.vmap(functools.partial(self._make_agent_observation, actions))(
+            jnp.arange(self.num_agents)
+        )
+        next_observation = Observation(
+            agent_obs=agent_obs,
+            step_count=steps,
+        )
+
+        timestep = jax.lax.cond(
+            done,
+            lambda: termination(
+                reward=rewards, observation=next_observation, shape=self.num_agents
+            ),
+            lambda: transition(
+                reward=rewards, observation=next_observation, shape=self.num_agents
+            ),
+        )
+
+        # create environment state
+        next_state = State(
+            step_count=steps,
+            key=state.key,
+        )
+        return next_state, timestep
+
+    def _make_agent_observation(
+        self,
+        actions: chex.Array,
+        agent_id: int,
+    ) -> chex.Array:
+        return jax.lax.cond(
+            self.keep_state,
+            lambda: actions,
+            lambda: jnp.zeros(self.num_agents, int),
+        )
+
+    def observation_spec(self) -> specs.Spec[Observation]:
+        """Specification of the observation of the MatrixGame environment.
+        Returns:
+            Spec for the `Observation`, consisting of the fields:
+                - agent_obs: BoundedArray (int32) of shape (num_agents, num_agents).
+                - step_count: BoundedArray (int32) of shape ().
+        """
+
+        obs_shape = (self.num_agents, self.num_agents)
+        low = jnp.zeros(obs_shape)
+        high = jnp.ones(obs_shape) * self.num_actions
+
+        agent_obs = specs.BoundedArray(obs_shape, jnp.int32, low, high, "agent_obs")
+        step_count = specs.BoundedArray((), jnp.int32, 0, self.time_limit, "step_count")
+        return specs.Spec(
+            Observation,
+            "ObservationSpec",
+            agent_obs=agent_obs,
+            step_count=step_count,
+        )
+
+    def action_spec(self) -> specs.MultiDiscreteArray:
+        """Returns the action spec.
+        Since this is a multi-agent environment, the environment expects an array of actions.
+        This array is of shape (num_agents,).
+        """
+        return specs.MultiDiscreteArray(
+            num_values=jnp.array([self.num_actions] * self.num_agents, jnp.int32),
+            name="action",
+        )
---
+++ matrax/utils/observation.py
@@ -0,0 +1,86 @@
+# Copyright 2023 InstaDeep Ltd. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+from typing import Tuple
+
+import chex
+import jax.numpy as jnp
+from jax import lax, random
+
+# def create_random_matrix(key_integer: int, num_agents: int, action_space: int) -> chex.Array:
+#     # Define the shape of the matrix
+#     shape = (action_space,) * num_agents
+
+#     key = random.PRNGKey(key_integer)
+#     random_matrix = random.uniform(key, shape=shape, minval=-1, maxval=1)
+
+#     max_reward = 11
+#     min_reward = -30
+
+#     # Generate a random postion on the full matrix for where to place the reward
+
+#     return random_matrix
+
+
+def create_random_matrix(
+    key_integrer: int, num_agents: int, action_space: int
+) -> Tuple[chex.Array, chex.Array, chex.Array]:
+    shape = (action_space,) * num_agents
+
+    subkey1, subkey2, subkey3 = random.split(random.PRNGKey(key_integrer), 3)
+
+    random_matrix = random.uniform(subkey1, shape=shape, minval=-1, maxval=1)
+
+    max_reward = 11
+    min_reward = -30
+
+    # Generate random indices for each axis for max reward
+    max_pos = random.randint(subkey2, (num_agents,), 0, action_space)
+
+    # Function to generate a new min_pos
+    def generate_min_pos(key: chex.PRNGKey) -> chex.Array:
+        return random.randint(key, (num_agents,), 0, action_space)
+
+    # Use lax.while_loop to ensure min_pos is different from max_pos
+    def cond_fun(carry: Tuple[chex.PRNGKey, chex.Array]) -> chex.Array:
+        key, min_pos = carry
+        return jnp.all(max_pos == min_pos)
+
+    def body_fun(
+        carry: Tuple[chex.PRNGKey, chex.Array]
+    ) -> Tuple[chex.PRNGKey, chex.Array]:
+        key, _ = carry
+        new_key, subkey = random.split(key)
+        return (new_key, generate_min_pos(subkey))
+
+    _, min_pos = lax.while_loop(
+        cond_fun, body_fun, (subkey3, generate_min_pos(subkey3))
+    )
+
+    # Place max and min rewards in the matrix
+    random_matrix = random_matrix.at[tuple(max_pos)].set(max_reward)
+    random_matrix = random_matrix.at[tuple(min_pos)].set(min_reward)
+
+    return random_matrix, max_pos, min_pos
+
+
+if __name__ == "__main__":
+
+    # Example usage
+    key_int = 2
+    num_agents = 4
+    action_space = 3
+    matrix, max_pos, min_pos = create_random_matrix(key_int, num_agents, action_space)
+    print(matrix.shape)
